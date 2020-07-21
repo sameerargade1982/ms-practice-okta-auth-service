e5dc399210d6942b502b511a0bbf3a24eaed450a
Sameer Argade <sameer.argade@dws.com.au>
2020-07-21 16:23:42 +1000

    intial commit

---
+++ src/main/java/com/argade/oktaauthservice/OktaauthserviceApplication.java
@@ -0,0 +1,13 @@
+package com.argade.oktaauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OktaauthserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OktaauthserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/argade/oktaauthservice/OktaauthserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.argade.oktaauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OktaauthserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
